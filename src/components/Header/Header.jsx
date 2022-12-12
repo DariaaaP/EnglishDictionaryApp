b089b4b920ca139ef899a8ceda4d1ed99c9ccb97
DariaaaP <barinova_f@list.ru>
2022-12-12 16:21:24 +0300

    fixed nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,24 +12,22 @@ import NotFoundPage from "../NotFoundPage/NotFoundPage.jsx";
 
 import style from './header.module.scss';
 
+const activeClass = ({ isActive }) => isActive ? style.active : style.menu;
+
 function Header() {
     return (
         <Router>
             <div className={style.container}>
                 <div className={style.nav}>
-                    <NavLink to='/' className={({ isActive }) =>
-                        (isActive ? (style.active) : style.menu)}>
+                    <NavLink to='/' className={activeClass}>
                         <div className={style.logo}>
                             <img className={style.img} src="assets/dictionary.png" alt="dict" />
                             <div>Dictionary App</div>
                         </div>
                     </NavLink>
-                    <NavLink to='/' className={({ isActive }) =>
-                        (isActive ? (style.active) : style.menu)}>Home</NavLink>
-                    <NavLink to='list' className={({ isActive }) =>
-                        (isActive ? (style.active) : style.menu)}>Words</NavLink>
-                    <NavLink to='/testwords' className={({ isActive }) =>
-                        (isActive ? (style.active) : style.menu)}>Game</NavLink>
+                    <NavLink to='/' className={activeClass}>Home</NavLink>
+                    <NavLink to='list' className={activeClass}>Words</NavLink>
+                    <NavLink to='/testwords' className={activeClass}>Game</NavLink>
                 </div>
             </div>
 
