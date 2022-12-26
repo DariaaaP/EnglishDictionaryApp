bdabf5cbcf8fb292664b1caa16fff1f9b70571fd
DariaaaP <barinova_f@list.ru>
2022-12-26 16:38:38 +0400

    changed the pages for storing Router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,28 @@
 import Header from '../Header/Header.jsx';
+import ListWords from "../ListWords/ListWords.jsx";
+import TestWords from "../TestWords/TestWords.jsx";
+import MainPage from "../Mainpage/MainPage.jsx";
+import NotFoundPage from "../NotFoundPage/NotFoundPage.jsx";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-    </div>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/list' element={<ListWords />} />
+          <Route path='/testwords' element={<TestWords />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </div >
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,23 @@
-
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    NavLink
-} from "react-router-dom";
-import ListWords from "../ListWords/ListWords.jsx";
-import TestWords from "../TestWords/TestWords.jsx";
-import MainPage from "../Mainpage/MainPage.jsx";
-import NotFoundPage from "../NotFoundPage/NotFoundPage.jsx";
-
+import { NavLink } from "react-router-dom";
 import style from './header.module.scss';
 
 const activeClass = ({ isActive }) => isActive ? style.active : style.menu;
 
 function Header() {
     return (
-        <Router>
-            <div className={style.container}>
-                <div className={style.nav}>
-                    <NavLink to='/' className={activeClass}>
-                        <div className={style.logo}>
-                            <img className={style.img} src="assets/dictionary.png" alt="dict" />
-                            <div>Dictionary App</div>
-                        </div>
-                    </NavLink>
-                    <NavLink to='/' className={activeClass}>Home</NavLink>
-                    <NavLink to='list' className={activeClass}>Words</NavLink>
-                    <NavLink to='/testwords' className={activeClass}>Game</NavLink>
-                </div>
+        <div className={style.container}>
+            <div className={style.nav}>
+                <NavLink to='/' className={activeClass}>
+                    <div className={style.logo}>
+                        <img className={style.img} src="assets/dictionary.png" alt="dict" />
+                        <div>Dictionary App</div>
+                    </div>
+                </NavLink>
+                <NavLink to='/' className={activeClass}>Home</NavLink>
+                <NavLink to='list' className={activeClass}>Words</NavLink>
+                <NavLink to='/testwords' className={activeClass}>Game</NavLink>
             </div>
-
-            <Routes>
-                <Route path='/' element={<MainPage />} />
-                <Route path='/list' element={<ListWords />} />
-                <Route path='/testwords' element={<TestWords />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-        </Router>
+        </div>
     );
 }
 
